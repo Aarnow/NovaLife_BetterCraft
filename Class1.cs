0bcfca8b5083ed74ab367eb4f1e79c5cb9e9b6df
Aarnow <arnaudfgr.dev@gmail.com>
2024-08-23 23:23:50 +0200

    Init

---
+++ Class1.cs
@@ -0,0 +1,21 @@
+﻿using Life;
+using ModKit.Helper;
+using ModKit.Interfaces;
+
+namespace BetterCraft
+{
+    public class BetterCraft : ModKit.ModKit
+    {
+        public BetterCraft(IGameAPI api) : base(api)
+        {
+            PluginInformations = new PluginInformations(AssemblyHelper.GetName(), "1.0.0", "Aarnow");
+        }
+
+        public override void OnPluginInit()
+        {
+            base.OnPluginInit();
+
+            ModKit.Internal.Logger.LogSuccess($"{PluginInformations.SourceName} v{PluginInformations.Version}", "initialisé");
+        }
+    }
+}
